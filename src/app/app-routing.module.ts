c30caf5ca166adddd5af125dd2c52743f6d8bf2a
Mario <mariomicetic@gmail.com>
2021-04-04 18:39:21 +0200

    1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'splash', loadChildren: () => import('./modules/splash/splash.module').then(m => m.SplashModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
