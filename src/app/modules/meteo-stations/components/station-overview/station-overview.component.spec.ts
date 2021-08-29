1636473bd895057f8296f3e7085e3f5454edf6ee
Mario <mariomicetic@gmail.com>
2021-08-29 23:10:42 +0200

    overview

--- src/app/modules/meteo-stations/components/station-overview/station-overview.component.spec.ts
+++ src/app/modules/meteo-stations/components/station-overview/station-overview.component.spec.ts
@@ -1,12 +1,116 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AngularFireModule } from "@angular/fire";
+import { By } from "@angular/platform-browser";
+import { BehaviorSubject } from "rxjs";
 import { environment } from "src/environments/environment";
 
 import { StationOverviewComponent } from './station-overview.component';
 
-describe('StationOverviewComponent', () => {
+fdescribe('StationOverviewComponent', () => {
   let component: StationOverviewComponent;
   let fixture: ComponentFixture<StationOverviewComponent>;
+
+  const testReadings = [
+    {
+      "timeStamp": {
+        "seconds": 1629839480,
+        "nanoseconds": 0
+      },
+      "value": 48.11322,
+      "sensorId": "G4sLf9DOaxBOmjGLmDU8",
+      "id": "4MNWUK4jinCY1VbFIeFy"
+    },
+    {
+      "timeStamp": {
+        "seconds": 1629740859,
+        "nanoseconds": 0
+      },
+      "value": 50.12436,
+      "sensorId": "G4sLf9DOaxBOmjGLmDU8",
+      "id": "6RGnLu2BO8VtdNWnelAN"
+    },
+    {
+      "sensorId": "G4sLf9DOaxBOmjGLmDU8",
+      "timeStamp": {
+        "seconds": 1629832280,
+        "nanoseconds": 0
+      },
+      "value": 48.0766,
+      "id": "DIsTeqhbcXczmNEjYBGM"
+    },
+    {
+      "value": 53.43252,
+      "sensorId": "G4sLf9DOaxBOmjGLmDU8",
+      "timeStamp": {
+        "seconds": 1629877745,
+        "nanoseconds": 0
+      },
+      "id": "FDLprwZ2UFwJ113ZruYA"
+    },
+    {
+      "value": 46.97337,
+      "sensorId": "G4sLf9DOaxBOmjGLmDU8",
+      "timeStamp": {
+        "seconds": 1629660320,
+        "nanoseconds": 0
+      },
+      "id": "PSn5vJCQHrvSV8GuHqbD"
+    },
+    {
+      "timeStamp": {
+        "seconds": 1629835880,
+        "nanoseconds": 0
+      },
+      "value": 48.00336,
+      "sensorId": "G4sLf9DOaxBOmjGLmDU8",
+      "id": "PiRmtWRbOaCZN5LWsykg"
+    },
+    {
+      "value": 47.43725,
+      "sensorId": "G4sLf9DOaxBOmjGLmDU8",
+      "timeStamp": {
+        "seconds": 1629750417,
+        "nanoseconds": 0
+      },
+      "id": "bNd9J6Sj3IoTSmeJnS3T"
+    },
+    {
+      "value": 49.61623,
+      "sensorId": "G4sLf9DOaxBOmjGLmDU8",
+      "timeStamp": {
+        "seconds": 1629663942,
+        "nanoseconds": 0
+      },
+      "id": "hLZqXOI2LUkAveBayItA"
+    },
+    {
+      "timeStamp": {
+        "seconds": 1629744459,
+        "nanoseconds": 0
+      },
+      "value": 47.62799,
+      "sensorId": "G4sLf9DOaxBOmjGLmDU8",
+      "id": "jgGrAdcKgOctJMVgztBN"
+    },
+    {
+      "value": 46.70787,
+      "sensorId": "G4sLf9DOaxBOmjGLmDU8",
+      "timeStamp": {
+        "seconds": 1629660342,
+        "nanoseconds": 0
+      },
+      "id": "kHbcVf2J0a4DXHjsQtZ6"
+    },
+    {
+      "timeStamp": {
+        "seconds": 1629746817,
+        "nanoseconds": 0
+      },
+      "value": 47.56542,
+      "sensorId": "G4sLf9DOaxBOmjGLmDU8",
+      "id": "ufq4vOOHiTLTT2pX3iam"
+    }
+  ]
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({imports: [
@@ -29,5 +133,22 @@ describe('StationOverviewComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should contain imgcard ', () => {
+    let card = fixture.debugElement.query(By.css('#imgCard'))
+    expect(card).toBeTruthy();
+  });
+
+  it('should contain detailsCard ', () => {
+    let card = fixture.debugElement.query(By.css('#detailsCard'))
+    expect(card).toBeTruthy();
+  });
+
+  it('should contain readingsCard ', () => {
+    component.readings$ = new BehaviorSubject(testReadings)
+    fixture.detectChanges();
+    let card = fixture.debugElement.query(By.css('#readingsCard'))
+    expect(card).toBeTruthy();
   });
 });
--- src/app/modules/readings/components/station-readings/station-readings.component.spec.ts
+++ src/app/modules/readings/components/station-readings/station-readings.component.spec.ts
@@ -1,6 +1,9 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
 import { AngularFireModule } from "@angular/fire";
 import { MatDialogRef, MAT_DIALOG_DATA } from "@angular/material/dialog";
+import { By } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { BehaviorSubject } from "rxjs";
 import { SharedModule } from "src/app/modules/shared/shared.module";
 import { environment } from "src/environments/environment";
 
@@ -9,6 +12,40 @@ import { StationReadingsComponent } from './station-readings.component';
 describe('StationReadingsComponent', () => {
   let component: StationReadingsComponent;
   let fixture: ComponentFixture<StationReadingsComponent>;
+  const testSensors = [
+    {
+      "description": "Vlažnost zraka",
+      "upperLimit": 100,
+      "stationId": "CQ9r2n0yXxrgX5RGFr1E",
+      "sensorTypeId": "lxyiTDDs1kPSiOqpF1sV",
+      "lowerLimit": 0,
+      "id": "G4sLf9DOaxBOmjGLmDU8"
+    },
+    {
+      "lowerLimit": 9000,
+      "sensorTypeId": "R28NgTu7xvjfUwfZyZk2",
+      "upperLimit": 11000,
+      "description": "Tlak zraka",
+      "stationId": "CQ9r2n0yXxrgX5RGFr1E",
+      "id": "J2WEQArcfOdR67NrhcVE"
+    },
+    {
+      "stationId": "CQ9r2n0yXxrgX5RGFr1E",
+      "sensorTypeId": "OX0leozGhNSo16rKuRxt",
+      "description": "UV index",
+      "lowerLimit": 0,
+      "upperLimit": 50,
+      "id": "LY9xZ38Jq0iM1A4N2QZy"
+    },
+    {
+      "description": "Temperatura zraka",
+      "lowerLimit": 0,
+      "upperLimit": 50,
+      "stationId": "CQ9r2n0yXxrgX5RGFr1E",
+      "sensorTypeId": "uyvDZzISG2iKybntKVwT",
+      "id": "j6jMOYDm3QJLkHrIrSLw"
+    }
+  ]
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -17,7 +54,7 @@ describe('StationReadingsComponent', () => {
         AngularFireModule.initializeApp(environment.firebase),
         SharedModule,
         //AppRoutingModule,
-        // BrowserAnimationsModule
+        BrowserAnimationsModule
       ],
       declarations: [ StationReadingsComponent ],
       providers: [
@@ -36,5 +73,12 @@ describe('StationReadingsComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('shows 4 expansion panels when there are 4 sensors', () => {
+    component.sensors$ = new BehaviorSubject(testSensors)
+    fixture.detectChanges();
+    let acordions = fixture.debugElement.queryAll(By.css('mat-expansion-panel'))
+    expect(acordions.length).toEqual(4);
   });
 });
